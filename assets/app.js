aef6540b6218ea2e41945256164d9bf9db6fc227
Michael Wolfe <michael.wolfe101@gmail.com>
2018-06-25 10:39:13 -0400

    styles

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@
 
 
 
-function playSound () {
+function playSound (e) {
     
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
